bdba6f21cfb4d219f1ce70aad967ac0e872857c1
Alfonso Salazar <alfonsomanuelsalazardeleon49@gmail.com>
2025-06-22 19:37:20 -0500

    Agregando la pagina de inicioAdmin al enrutador

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/user/Register";
-
+import InicioAdmin from "../pages/admin/InicioAdmin";
+  
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<InicioAdmin />} />
       </Routes>
     </BrowserRouter>
   );
